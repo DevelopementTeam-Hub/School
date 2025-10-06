b1bb163991274de4524e05bcfba7185d6f8f76e2
jagadeesh laveti <jagadeesh laveti@jagapatibabu>
2025-10-06 22:31:05 +0530

    commit

--- SchoolManagementSystem/src/main/java/com/school/SchoolManagementSystemApplication.java
+++ SchoolManagementSystem/src/main/java/com/school/SchoolManagementSystemApplication.java
@@ -11,7 +11,7 @@ public class SchoolManagementSystemApplication {
 	public static void main(String[] args) {
 		 SpringApplication.run(SchoolManagementSystemApplication.class, args);
 		 }
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
 		
